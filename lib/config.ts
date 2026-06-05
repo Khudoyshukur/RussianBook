8d42be197f86de30ce0dcb348d28d23b31854cf1
kjuraev <kjuraev.001@gmail.com>
2026-06-05 15:30:57 +0500

    Disable test mode

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,7 @@
  * TEST_MODE: When true, all lessons are unlocked regardless of progress
  * Set to false in production to require sequential completion
  */
-export const TEST_MODE = true;
+export const TEST_MODE = false;
 
 /**
  * Total number of lessons in the course
